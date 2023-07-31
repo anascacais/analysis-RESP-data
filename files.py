7bc5a99bbba76a8bbd42535dc5ec6c57dc6f02b8
anascacais <anascacais@gmail.com>
2023-07-31 11:16:48 +0100

    compute dataset characteristics

--- files.py
+++ files.py
@@ -6,7 +6,7 @@ import pickle
 # third party
 import pandas as pd
 
-def load_data(directory, id):
+def load_data(directory, id, resp_only=True):
 
     '''  brief description 
     
@@ -27,6 +27,13 @@ def load_data(directory, id):
     scientisst_data = pd.read_csv(os.path.join(directory, id, f'scientisst_{id}.csv'), sep=',',  skiprows=2, index_col=False, names=["NSeq", "ECG", "ACC1", "ACC2", "ACC3", "LED", "RESP"], usecols=["ECG", "ACC1", "ACC2", "ACC3", "LED", "RESP"])
     bitalino_data = pd.read_csv(os.path.join(directory, id, f'bitalino_{id}.txt'), sep='\t',  skiprows=3, index_col=False, names=["nSeq", "I1", "I2", "O1", "O2", "PZT", "LUX", "A3", "A4", "A5", "A6"], usecols=["PZT", "LUX"])
     
+    scientisst_data.RESP = -scientisst_data.RESP
+    
+    if resp_only:
+        biopac_data = biopac_data[["airflow"]]
+        scientisst_data = scientisst_data[["RESP"]]
+        bitalino_data = bitalino_data[["PZT"]]
+
     
     with open(os.path.join(directory, id, f'idx_{id}.json'), "r") as jsonFile:
         activities_info = json.load(jsonFile)
@@ -34,16 +41,15 @@ def load_data(directory, id):
     return scientisst_data, biopac_data, bitalino_data, activities_info
 
 
-def save_results(save_directory, participant_results):
+def save_results(participant_results, id):
     
-    try: 
-        with open(os.path.join(save_directory, 'results.pickle'), 'rb') as file:
-            results = pickle.load(file)     
-    
-    except FileNotFoundError:
+    try:
+        results = pickle.load(open("Results/results.pickle", "rb"))
+    except (OSError, IOError):
         results = {}
     
     finally:
         results[id] = participant_results
+
         with open('Results/results.pickle', 'wb') as file:
             pickle.dump(results, file)
